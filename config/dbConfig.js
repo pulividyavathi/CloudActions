3163ecedfeec2ad5ace57f5acb7e7242e27d5f8e
Vidyavathi Puli <puli.v@northeastern.edu>
2023-10-05 11:55:42 -0400

    Action Integration worked

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,25 +1,30 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
+// // const { Sequelize } = require('sequelize');
+// // require('dotenv').config();
 
-// const sequelize = new Sequelize(
+// // const sequelize = new Sequelize(
+// //   process.env.DB_NAME,
+// //   process.env.DB_USERNAME,
+// //   process.env.DB_PASSWORD,
+// //   {
+// //     dialect: 'mysql',
+// //     host: process.env.DB_HOST,
+// //   }
+// // );
+
+// // module.exports = sequelize;
+const { Sequelize } = require('sequelize');
+
+// module.exports = new Sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_USERNAME,
 //   process.env.DB_PASSWORD,
 //   {
-//     dialect: 'mysql',
 //     host: process.env.DB_HOST,
+//     dialect: 'mysql',  
 //   }
 // );
-
-// module.exports = sequelize;
-const { Sequelize } = require('sequelize');
-
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',  
-  }
-);
+const Sequelize = new Sequelize('csye6225', 'root', 'Writecode@123', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+module.exports = Sequelize
