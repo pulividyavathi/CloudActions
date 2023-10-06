e89dc9167b8c43b1a52c07b034066f775aaccfce
Vidyavathi Puli <puli.v@northeastern.edu>
2023-10-05 22:41:24 -0400

    test

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -14,17 +14,17 @@
 // // module.exports = sequelize;
   const { Sequelize } = require('sequelize');
 
-//  const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql',  
-//   }
-// );
-const sequelize = new Sequelize('csye6225', 'root', 'Writecode@123', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+ const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',  
+  }
+);
+// const sequelize = new Sequelize('csye6225', 'root', 'Writecode@123', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
 module.exports = sequelize
